53593a7d1ea1f8c1a7c20aab494dc4ea33aedd40
Jacob Kjeldahl <jacob@kjeldahl.it>
2023-12-03 12:02:03 +0100

    Day 02 Second star

--- 02/cube.rb
+++ 02/cube.rb
@@ -18,3 +18,13 @@ first_star =
         sum {|s| s[:id]}
 
 puts "First star: #{first_star}"
+
+second_star =
+    lines.
+        map {|l| l.match /Game (?<id>\d+): (?<data>.*)/ }.
+        map {|m| {id: m[:id].to_i, sets: m[:data].split(";").map(&:strip)}}.
+        map {|s| {id: s[:id], sets: s[:sets].map{|set| set_to_h(set) }}}.
+        map {|s| s[:sets].inject({}){|acc, set| set.each{|col, count| acc[col]=count if acc[col].nil? || acc[col] < count} ; acc } }.
+        sum {|s| s.values.reduce(:*)}
+
+puts "Second star: #{second_star}"
